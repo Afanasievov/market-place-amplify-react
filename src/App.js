4bfebb258d31312edbe9ea1f57333dd524249a50
Oleksandr Afanasiev <o.afanasiev@mobidev.biz>
2020-05-13 21:03:20 +0300

    change styling of withAuthenticator component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from 'react';
-import { withAuthenticator } from 'aws-amplify-react';
+import { withAuthenticator, AmplifyTheme } from 'aws-amplify-react';
 import '@aws-amplify/ui/dist/style.css';
 
 function App() {
   return <div>App</div>;
 }
 
-export default withAuthenticator(App);
+const theme = {
+  ...AmplifyTheme,
+  navBar: {
+    ...AmplifyTheme.navBar,
+    backgroundColor: '#006666',
+  },
+  button: {
+    ...AmplifyTheme.button,
+    backgroundColor: 'var(--darkAmazonOrange)',
+  },
+  sectionBody: {
+    ...AmplifyTheme.sectionBody,
+    padding: '5px',
+  },
+  sectionHeader: {
+    ...AmplifyTheme.sectionHeader,
+    backgroundColor: 'var(--squidInk)',
+  },
+};
+
+export default withAuthenticator(App, true, [], null, theme);
