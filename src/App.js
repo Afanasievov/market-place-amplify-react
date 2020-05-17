9a31e47e1c4bfe402ccbb682a0a8e112d59c95bf
Oleksandr Afanasiev <afanasievov@gmail.com>
2020-05-17 13:44:26 +0300

    replace withAuthenticator component with  Authenticator

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import { withAuthenticator, AmplifyTheme } from 'aws-amplify-react';
+import React, { useState, useEffect } from 'react';
+import { Authenticator, AmplifyTheme } from 'aws-amplify-react';
+import { Auth, Hub, Logger } from 'aws-amplify';
 import '@aws-amplify/ui/dist/style.css';
 
-function App() {
-  return <div>App</div>;
-}
+const logger = new Logger('App.js', process.env === 'production' ? 'INFO' : 'DEBUG');
 
 const theme = {
   ...AmplifyTheme,
@@ -26,4 +25,48 @@ const theme = {
   },
 };
 
-export default withAuthenticator(App, true, [], null, theme);
+function App() {
+  const [user, setUser] = useState(null);
+
+  const getUserData = async () => {
+    try {
+      const authenticated = await Auth.currentAuthenticatedUser();
+      setUser(authenticated);
+    } catch (error) {
+      setUser(null);
+    }
+  };
+
+  useEffect(() => {
+    getUserData();
+  }, []);
+
+  useEffect(() => {
+    const onHubCapsule = (capsule) => {
+      switch (capsule.payload.event) {
+        case 'signIn':
+          logger.debug('onHubCapsule -> signIn');
+          getUserData();
+          break;
+        case 'signUp':
+          logger.debug('onHubCapsule -> signUp');
+          break;
+        case 'signOut':
+          logger.debug('onHubCapsule -> signOut');
+          setUser(null);
+          break;
+        default:
+      }
+    };
+
+    Hub.listen('auth', (data) => {
+      onHubCapsule(data);
+    });
+
+    return Hub.remove('auth');
+  }, []);
+
+  return user ? <div>App</div> : <Authenticator theme={theme} />;
+}
+
+export default App;
