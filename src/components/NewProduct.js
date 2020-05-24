1de9621cb05202f507f72a559dda951b6ce89eed
Oleksandr Afanasiev <afanasievov@gmail.com>
2020-05-24 13:54:30 +0300

    style PhotoPicker Component

--- src/components/NewProduct.js
+++ src/components/NewProduct.js
@@ -7,9 +7,14 @@ const NewProduct = () => {
   const [description, setDescription] = useState('');
   const [price, setPrice] = useState('');
   const [shipped, setShipped] = useState(false);
+  const [imagePreview, setImagePreview] = useState('');
+  const [image, setImage] = useState('');
 
   const handleAddProduct = () => {
-    console.log('product');
+    console.log('NewProduct -> description', description);
+    console.log('NewProduct -> price', price);
+    console.log('NewProduct -> shipped', shipped);
+    console.log('NewProduct -> image', image);
   };
 
   return (
@@ -22,6 +27,7 @@ const NewProduct = () => {
               type="text"
               icon="information"
               placeholder="Description"
+              value={description}
               onChange={(text) => setDescription(text)}
             />
           </Form.Item>
@@ -30,6 +36,7 @@ const NewProduct = () => {
               type="number"
               icon="plus"
               placeholder="Price ($USD)"
+              value={price}
               onChange={(num) => setPrice(num)}
             />
           </Form.Item>
@@ -43,9 +50,44 @@ const NewProduct = () => {
               </Radio>
             </div>
           </Form.Item>
-          <PhotoPicker />
+          {imagePreview && <img className="image-preview" src={imagePreview} alt="Image Preview" />}
+          <PhotoPicker
+            title="Product Image"
+            preview="hidden"
+            onLoad={(url) => setImagePreview(url)}
+            onPick={(file) => setImage(file)}
+            theme={{
+              formContainer: {
+                margin: 0,
+                padding: '0.8em',
+              },
+              formSection: {
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+              },
+              sectionBody: {
+                margin: 0,
+                width: '250px',
+              },
+              sectionHeader: {
+                padding: '0.2em',
+                color: 'var(--darkAmazonOrange)',
+              },
+              photoPickerButton: {
+                // display: 'none',
+              },
+            }}
+          />
           <Form.Item>
-            <Button type="primary" onClick={handleAddProduct}>Add Product</Button>
+            <Button
+              disable={!image || !description || !price}
+              type="primary"
+              onClick={handleAddProduct}
+            >
+              Add Product
+            </Button>
           </Form.Item>
         </Form>
       </div>
