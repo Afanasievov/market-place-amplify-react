e9c6e250c99722d1a1f1b35c64331757e5b5e8f8
Oleksandr Afanasiev <afanasievov@gmail.com>
2020-05-25 22:09:44 +0300

    update products

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,42 +1,149 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { API, graphqlOperation } from 'aws-amplify';
 import { S3Image } from 'aws-amplify-react';
-import { Card } from 'element-react';
-import { convertCentsToDollars } from '../utils';
+import { Card, Button, Dialog, Form, Input, Radio, Notification } from 'element-react';
+import { convertCentsToDollars, convertDollarsToCents } from '../utils';
 import { UserContext } from '../App';
 import PayButton from './PayButton';
+import { updateProduct } from '../graphql/mutations';
 
-const Product = ({ product }) => (
-  <UserContext.Consumer>
-    {({ user }) => {
-      const isProductOwner = user && user.attributes.sub === product.owner;
+const Product = ({ product }) => {
+  const [updateProductDialog, setUpdateProductDialog] = useState(false);
+  const [description, setDescription] = useState('');
+  const [price, setPrice] = useState('');
+  const [shipped, setShipped] = useState(true);
 
-      return (
-        <div className="card-container">
-          <Card bodyStyle={{ padding: 0, minWidth: '200px' }}>
-            <S3Image
-              imgKey={product.file.key}
-              theme={{ photoImg: { maxWidth: '100%', maxHeight: '100%' } }}
-            />
-            <div className="card-body">
-              <h3 className="m-0">{product.description}</h3>
-              <div className="items-center">
-                <img
-                  src={`https:icon.now.sh/${product.shipped ? 'markunread_mailbox' : 'mail'}`}
-                  alt="Shipping Icon"
-                  className="icon"
-                />
-                {product.shipped ? 'Shipped' : 'Emailed'}
+  const handleUpdateProduct = async (id) => {
+    try {
+      setUpdateProductDialog(false);
+      const input = {
+        id,
+        description,
+        shipped,
+        price: convertDollarsToCents(price),
+      };
+      const duration = 2000;
+
+      await API.graphql(graphqlOperation(updateProduct, { input }));
+      Notification({
+        title: 'Success',
+        message: 'Product successfully updated!',
+        type: 'success',
+        duration,
+      });
+      setTimeout(() => {
+        window.location.reload();
+      }, duration);
+    } catch (error) {
+      console.error('handleUpdateProduct -> error', error);
+    }
+  };
+
+  return (
+    <UserContext.Consumer>
+      {({ user }) => {
+        const isProductOwner = user && user.attributes.sub === product.owner;
+
+        return (
+          <div className="card-container">
+            <Card bodyStyle={{ padding: 0, minWidth: '200px' }}>
+              <S3Image
+                imgKey={product.file.key}
+                theme={{ photoImg: { maxWidth: '100%', maxHeight: '100%' } }}
+              />
+              <div className="card-body">
+                <h3 className="m-0">{product.description}</h3>
+                <div className="items-center">
+                  <img
+                    src={`https:icon.now.sh/${product.shipped ? 'markunread_mailbox' : 'mail'}`}
+                    alt="Shipping Icon"
+                    className="icon"
+                  />
+                  {product.shipped ? 'Shipped' : 'Emailed'}
+                </div>
+                <div className="text-right">
+                  $<span className="mx-1">{convertCentsToDollars(product.price)}</span>
+                  {!isProductOwner && <PayButton />}
+                </div>
               </div>
-              <div className="text-right">
-                $<span className="mx-1">{convertCentsToDollars(product.price)}</span>
-                {!isProductOwner && <PayButton />}
-              </div>
+            </Card>
+            <div className="text-center">
+              {isProductOwner && (
+                <>
+                  <Button
+                    type="warning"
+                    icon="edit"
+                    className="m-1"
+                    onClick={() => {
+                      setUpdateProductDialog(true);
+                      setDescription(product.description);
+                      setShipped(product.shipped);
+                      setPrice(convertCentsToDollars(product.price));
+                    }}
+                  />
+                  <Button type="danger" icon="delete" />
+                </>
+              )}
             </div>
-          </Card>
-        </div>
-      );
-    }}
-  </UserContext.Consumer>
-);
+            <Dialog
+              title="Update Product"
+              size="large"
+              customClass="dialog"
+              visible={updateProductDialog}
+              onCancel={() => setUpdateProductDialog(false)}
+            >
+              <Dialog.Body>
+                <Form labelPosition="top">
+                  <Form.Item label="Update Description">
+                    <Input
+                      placeholder="Product Description"
+                      icon="information"
+                      trim
+                      value={description}
+                      onChange={(text) => setDescription(text)}
+                    />
+                  </Form.Item>
+                  <Form.Item label="Product Price">
+                    <Input
+                      type="number"
+                      icon="plus"
+                      placeholder="Price ($USD)"
+                      value={price}
+                      onChange={(num) => setPrice(num)}
+                    />
+                  </Form.Item>
+                  <Form.Item label="Update Shipping">
+                    <div className="text-center">
+                      <Radio
+                        value="true"
+                        checked={shipped === true}
+                        onChange={() => setShipped(true)}
+                      >
+                        Shipped
+                      </Radio>
+                      <Radio
+                        value="false"
+                        checked={shipped === false}
+                        onChange={() => setShipped(false)}
+                      >
+                        Emailed
+                      </Radio>
+                    </div>
+                  </Form.Item>
+                </Form>
+              </Dialog.Body>
+              <Dialog.Footer>
+                <Button onClick={() => setUpdateProductDialog(false)}>Cancel</Button>
+                <Button type="primary" onClick={() => handleUpdateProduct(product.id)}>
+                  Update
+                </Button>
+              </Dialog.Footer>
+            </Dialog>
+          </div>
+        );
+      }}
+    </UserContext.Consumer>
+  );
+};
 
 export default Product;
