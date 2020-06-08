d4a0086504f4c64d235ccce1704fda0fb2c47100
Oleksandr Afanasiev <afanasievov@gmail.com>
2020-06-08 12:58:49 +0300

    register new users on signIn

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { Authenticator, AmplifyTheme } from 'aws-amplify-react';
-import { Auth, Hub, Logger } from 'aws-amplify';
+import { Auth, API, Hub, Logger, graphqlOperation } from 'aws-amplify';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import 'element-theme-default';
 import HomePage from './pages/HomePage';
 import MarketPage from './pages/MarketPage';
 import ProfilePage from './pages/ProfilePage';
 import Navbar from './components/Navbar';
+import { getUser } from './graphql/queries';
+import { registerUser } from './graphql/mutations';
 
 import '@aws-amplify/ui/dist/style.css';
 import './App.css';
@@ -51,12 +53,33 @@ const App = () => {
     getUserData();
   }, []);
 
+  const registerNewUser = async (signInData) => {
+    const getUserInput = {
+      id: signInData.signInUserSession.idToken.payload.sub,
+    };
+    const { data } = await API.graphql(graphqlOperation(getUser, getUserInput));
+    if (!data.getUser) {
+      try {
+        const registerUserInput = {
+          ...getUserInput,
+          username: signInData.username,
+          email: signInData.signInUserSession.idToken.payload.email,
+          registered: true,
+        };
+        await API.graphql(graphqlOperation(registerUser, { input: registerUserInput }));
+      } catch (error) {
+        logger.error(error);
+      }
+    }
+  };
+
   useEffect(() => {
     const onHubCapsule = (capsule) => {
       switch (capsule.payload.event) {
         case 'signIn':
           logger.debug('onHubCapsule -> signIn');
           getUserData();
+          registerNewUser(capsule.payload.data);
           break;
         case 'signUp':
           logger.debug('onHubCapsule -> signUp');
